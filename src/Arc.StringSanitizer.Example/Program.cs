fb155633b073266ed25dad305355264c0cd3acf1
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-08-09 19:24:26 +0530

    save

---
+++ src/Arc.StringSanitizer.Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using Arc.StringSanitizer;
+using static System.Console;
+
+var unsanitizedString = "sample test with special char. some more &nbsp; char";
+
+var sanitizerConfigs = new List<SanitizerConfig>()
+{
+    new SanitizerConfig("special char", "[sc]"),
+    new SanitizerConfig("&nbsp;", "[html space]")
+};
+
+var sanitizedString = unsanitizedString.Sanitize(sanitizerConfigs);
+WriteLine(sanitizedString); // Output: sample test with [sc]. some more [html space] char
+
+var newUnsanitizedString = sanitizedString.Unsanitize(sanitizerConfigs);
+WriteLine(newUnsanitizedString); // Output: sample test with special char. some more &nbsp; char
