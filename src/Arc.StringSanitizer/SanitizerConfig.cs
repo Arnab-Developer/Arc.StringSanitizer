b6f06fb064cee1d315d0650f406452eebefe4510
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-08-09 21:53:31 +0530

    test

--- src/Arc.StringSanitizer/SanitizerConfig.cs
+++ src/Arc.StringSanitizer/SanitizerConfig.cs
@@ -14,7 +14,7 @@ public class SanitizerConfig
     /// <param name="to">String replaced with.</param>
     public SanitizerConfig(string from, string to)
     {
-        From = from;
+        From = from;//test
         To = to;
     }
 }
