e34049cb51d816d6605292b7876abb4e48301be0
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-08-09 14:47:56 +0530

    save

--- test/Arc.StringSanitizerTest/StringSanitizerExtensionsTest.SanitizeSingle.cs
+++ test/Arc.StringSanitizerTest/StringSanitizerExtensionsTest.SanitizeSingle.cs
@@ -43,4 +43,48 @@ public partial class StringSanitizerExtensionsTest
         var ex = Assert.Throws<ArgumentException>(func);
         Assert.Equal("'input' cannot be null or empty. (Parameter 'input')", ex.Message);
     }
+
+    [Fact]
+    public void SanitizeSingle_ThrowException_WhenInputIsEmpty()
+    {
+        // Arrange
+        var unsanitizedString = "";
+
+        // Act.
+        string func() => unsanitizedString.Sanitize(_sanitizerConfig);
+
+        // Assert.
+        var ex = Assert.Throws<ArgumentException>(func);
+        Assert.Equal("'input' cannot be null or empty. (Parameter 'input')", ex.Message);
+    }
+
+    [Fact]
+    public void SanitizeSingle_ThrowException_WhenInputIsBlankSpace()
+    {
+        // Arrange
+        var unsanitizedString = " ";
+
+        // Act.
+        string func() => unsanitizedString.Sanitize(_sanitizerConfig);
+
+        // Assert.
+        var ex = Assert.Throws<ArgumentException>(func);
+        Assert.Equal("'input' cannot be null or empty. (Parameter 'input')", ex.Message);
+    }
+
+    [Fact]
+    public void SanitizeSingle_ThrowException_WhenConfigIsNull()
+    {
+        // Arrange
+        var unsanitizedString = "sample test with special char. some more &nbsp; char";
+        SanitizerConfig? sanitizerConfig = null;
+
+        // Act.
+#pragma warning disable CS8604 // Possible null reference argument.
+        string func() => unsanitizedString.Sanitize(sanitizerConfig);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+        // Assert.
+        Assert.Throws<NullReferenceException>(func);
+    }
 }
