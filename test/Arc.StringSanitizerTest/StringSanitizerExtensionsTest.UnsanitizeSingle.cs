1bb6fa5ff6c66b92d55a2e8c70b4ed51744d6f34
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-08-09 15:28:01 +0530

    save

---
+++ test/Arc.StringSanitizerTest/StringSanitizerExtensionsTest.UnsanitizeSingle.cs
@@ -0,0 +1,17 @@
+﻿namespace Arc.StringSanitizerTest;
+
+public partial class StringSanitizerExtensionsTest
+{
+    [Fact]
+    public void UnsanitizeSingle_UnsanitizeProperly_WhenStringIsFound()
+    {
+        // Arrange.
+        var sanitizedString = "sample test with [sc]. some more [html space] char";
+
+        // Act.
+        var unsanitizedString = sanitizedString.Unsanitize(_sanitizerConfig);
+
+        // Assert.
+        Assert.Equal("sample test with special char. some more [html space] char", unsanitizedString);
+    }
+}
